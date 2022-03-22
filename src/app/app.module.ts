ddb0698441d401fc38f905a18499daa6cf553c86
Anton Semenov <semenov.anton.a@gmail.com>
2022-03-22 18:34:49 +0200

    Fake API

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
 
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 
@@ -11,28 +12,29 @@ import { AppComponent } from './app.component';
 import { IndexComponent } from './pages/index/index.component';
 import { DetailsComponent } from './pages/details/details.component';
 
-import {CategoryListComponent} from './components/category-list/category-list.component';
+import { CategoryListComponent } from './components/category-list/category-list.component';
 import { CardsComponent } from './components/cards/cards.component';
 import { LangsComponent } from './components/langs/langs.component';
 
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    IndexComponent,
-    CategoryListComponent,
-    DetailsComponent,
-    CardsComponent,
-    LangsComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule,
-    FontAwesomeModule    
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [
+        AppComponent,
+        IndexComponent,
+        CategoryListComponent,
+        DetailsComponent,
+        CardsComponent,
+        LangsComponent
+    ],
+    imports: [
+        BrowserModule,
+        AppRoutingModule,
+        FontAwesomeModule,
+        HttpClientModule
+    ],
+    providers: [],
+    bootstrap: [AppComponent]
 })
-export class AppModule { 
-   
- }
+export class AppModule {
+
+}
