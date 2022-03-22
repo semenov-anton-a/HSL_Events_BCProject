bb0ea14dc46feda4a4ae44754b78b99537283cf5
Anton Semenov <semenov.anton.a@gmail.com>
2022-03-22 14:49:30 +0200

    Maked Routers

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+
+import { IndexComponent , DetailsComponent } from './pages';
+
+const routes: Routes = [
+    { path: 'd', component: DetailsComponent },
+    { path: '', component: IndexComponent },
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  imports: [ RouterModule.forRoot(routes) ],
+  exports: [ RouterModule ]
 })
 export class AppRoutingModule { }
