e8a54867e0912f214f1d0ba67e85a3233baf419a
Anton Semenov <semenov.anton.a@gmail.com>
2022-03-22 15:54:49 +0200

    Tempalte v1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+
+
 import { IndexComponent } from './pages/index/index.component';
 import { DetailsComponent } from './pages/details/details.component';
+
+import {CategoryListComponent} from './pages/index/category-list/category-list.component';
+import { CardsComponent } from './pages/index/cards/cards.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
     IndexComponent,
-    DetailsComponent
+    CategoryListComponent,
+    DetailsComponent,
+    CardsComponent
   ],
   imports: [
     BrowserModule,
