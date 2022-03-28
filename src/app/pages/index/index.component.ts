7d359edeb58f6b0722b177a0e9490f9773ba8b4f
Anton Semenov <semenov.anton.a@gmail.com>
2022-03-28 09:38:39 +0300

    Fake API

--- src/app/pages/index/index.component.ts
+++ src/app/pages/index/index.component.ts
@@ -1,16 +1,22 @@
 import { Component, OnInit } from '@angular/core';
+import { AlldataService } from 'src/app/services/alldata.service';
 
 @Component({
-  selector: 'app-index',
-  templateUrl: './index.component.html',
-  styleUrls: ['./index.component.css']
+    selector: 'app-index',
+    templateUrl: './index.component.html',
+    styleUrls: ['./index.component.css']
 })
 
 export class IndexComponent implements OnInit {
 
-  constructor() { }
+    data = [];
 
-  ngOnInit(): void {
-  }
+    constructor( private allData : AlldataService ) { }
+
+    ngOnInit(): void {
+
+        this.allData.getAlldata().subscribe( (res : any) => {} );
+
+    }
 
 }
---
+++ src/app/services/alldata.service.ts
@@ -0,0 +1,40 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { catchError, map, tap } from 'rxjs/operators';
+import { Observable } from 'rxjs';
+
+@Injectable({ providedIn: 'root' })
+export class AlldataService {
+
+    // private httpOptions = {
+    //     headers: new HttpHeaders({ 'Content-Type': 'application/json' })
+    // };
+
+    constructor(
+        private http: HttpClient
+    ) { }
+
+
+    getAlldata(){
+
+        // console.log( "getAllProducts")
+
+        // let url = "https://open-api.myhelsinki.fi/v2/places/?language_filter=en";
+        
+        let url = "./assets/json/places.json";
+
+        const json = this.http.get<any[]>( url )
+            .pipe( 
+                tap( (response) => { console.log(response) } )
+             );
+             
+        return json;
+
+        // return this.http.get("");
+
+    }
+
+
+
+
+}
