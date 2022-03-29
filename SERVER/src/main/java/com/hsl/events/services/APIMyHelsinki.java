e0b2d0caa71575a4f546a1e6a1411b21f2a56314
Anton Semenov <semenov.anton.a@gmail.com>
2022-03-29 15:10:44 +0300

    Make SERVER

---
+++ SERVER/src/main/java/com/hsl/events/services/APIMyHelsinki.java
@@ -0,0 +1,49 @@
+package com.hsl.events.services;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class APIMyHelsinki {
+
+    private String apiUrl = "http://open-api.myhelsinki.fi/";
+    
+
+
+
+
+
+
+    public String get( String url ) throws IOException 
+    {    
+        StringBuilder result = new StringBuilder();
+        
+        String line;
+
+        HttpURLConnection urlConnection = (HttpURLConnection) new URL(url).openConnection();
+        
+        urlConnection.setReadTimeout(5000);
+        urlConnection.setConnectTimeout(5000);
+        urlConnection.setUseCaches(true);
+
+        try (
+            InputStream is = new URL(url).openStream();
+            BufferedReader br = new BufferedReader(new InputStreamReader(is)) 
+        ){
+
+            while ((line = br.readLine()) != null) {
+                result.append(line);
+            }
+
+        }
+
+        return result.toString();
+    }
+    // END
+}
