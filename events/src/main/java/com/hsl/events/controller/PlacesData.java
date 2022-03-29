80a8db2df28f6f461bd09a6c53a6f81753c9d527
Anton Semenov <semenov.anton.a@gmail.com>
2022-03-29 14:42:58 +0300

    API

---
+++ events/src/main/java/com/hsl/events/controller/PlacesData.java
@@ -0,0 +1,14 @@
+package com.hsl.events.controller;
+
+import java.util.ArrayList;
+import java.util.Map;
+
+import lombok.Data;
+
+@Data
+public class PlacesData {
+
+    Map< ArrayList<String>, ArrayList<String> > data;
+
+
+}
