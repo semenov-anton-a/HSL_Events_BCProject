55498dbfe2046ad15fa76785ef4690af71bdab4a
Anton Semenov <semenov.anton.a@gmail.com>
2022-04-01 17:00:06 +0300

    Added Activities Controller

--- server/src/main/java/com/hsl/server/controllers/ApiActivitiesController.java
+++ server/src/main/java/com/hsl/server/controllers/ApiActivitiesController.java
@@ -1,32 +1,27 @@
 package com.hsl.server.controllers;
 
 import java.io.IOException;
-import java.net.URL;
-import java.util.ArrayList;
 import java.util.Map;
 
 import javax.servlet.http.HttpServletRequest;
 
 import com.hsl.server.APIMyHelsinkiService;
-
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 // @RequestMapping(headers = "Accept=application/json", produces = "text/plain;charset=UTF-8")
 @RestController
-public class ApiPlacesController {
+public class ApiActivitiesController {
     
 
-
-
-    @RequestMapping( value = "/api/places" )
+    @RequestMapping( value = "/api/activities" )
     public String getApi( 
         HttpServletRequest request,
         @RequestParam Map<String,String> requestParams 
     ) throws IOException {
-
-        return APIMyHelsinkiService.getDataByCategoryAPIVersion2( "places", request, requestParams );
+        
+        return APIMyHelsinkiService.getDataByCategoryAPIVersion2( "activities", request, requestParams );
     
     }
 
