e6f1c166bd75005586b22b0bd7702329825ee13b
Anton Semenov <semenov.anton.a@gmail.com>
2022-04-08 03:04:05 +0300

    up

--- server/src/main/java/com/hsl/server/JsonFetcher.java
+++ server/src/main/java/com/hsl/server/JsonFetcher.java
@@ -3,7 +3,6 @@ package com.hsl.server;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.net.MalformedURLException;
 import java.net.URL;
 import java.net.URLConnection;
 import java.util.zip.GZIPInputStream;
--- server/src/main/java/com/hsl/server/controllers/ApiPlacesController.java
+++ server/src/main/java/com/hsl/server/controllers/ApiPlacesController.java
@@ -1,8 +1,6 @@
 package com.hsl.server.controllers;
 
 import java.io.IOException;
-import java.net.URL;
-import java.util.ArrayList;
 import java.util.Map;
 
 import javax.servlet.http.HttpServletRequest;
