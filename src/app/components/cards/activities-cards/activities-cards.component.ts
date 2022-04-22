9b5740e5c55e8cf5b4052b45bfbacd9c8bf86290
Anton Semenov <semenov.anton.a@gmail.com>
2022-04-22 19:00:48 +0300

    All Languages in a " Activities "

--- src/app/components/cards/activities-cards/activities-cards.component.ts
+++ src/app/components/cards/activities-cards/activities-cards.component.ts
@@ -8,77 +8,105 @@ import { LangService } from 'src/app/services/lang.service';
 
 
 @Component({
-  selector: 'app-activities-cards',
-  templateUrl: './activities-cards.component.html',
-  styleUrls: ['./activities-cards.component.css']
+    selector: 'app-activities-cards',
+    templateUrl: './activities-cards.component.html',
+    styleUrls: ['./activities-cards.component.css']
 })
 export class ActivitiesCardsComponent implements OnInit {
 
-    public readonly maxTitleLength:  number = 50;
-    public readonly maxDescriptionLength:  number = 500;
+    public readonly maxTitleLength: number = 50;
+    public readonly maxDescriptionLength: number = 500;
 
     faCoffee = faCoffee;
     faStar = faStar;
     faEarthAmerica = faEarthAmerica;
 
-    @ViewChild(NgxMasonryComponent) masonry: NgxMasonryComponent | any ;
+    @ViewChild(NgxMasonryComponent) masonry: NgxMasonryComponent | any;
 
     @Input() cardsData: any[] | undefined;
 
-
-
     private currentLanguage = this.langService.getLanguage();
-    constructor(private langService: LangService) {
-        // console.log( this.currentLanguage )
-        // console.log( this.langService.getLanguage() )
+
+    constructor(private langService: LangService) { }
+
+
+    ngOnInit(): void {
+        this.langService.getObsData().subscribe((lang: any) => {
+            this.currentLanguage = lang;
+        })
+
+        //    setInterval( () => { 
+        //         this.masonry.reloadItems();
+        //         this.masonry.layout();  
+        //     }, 500)
     }
 
 
-    reloadItems(){
+    reloadItems() {
         // if( this.masonry !== undefined){
         //     this.masonry.reloadItems();
         //     this.masonry.layout();
         //     // return ;
         // }
 
-        setTimeout(() => { 
+        setTimeout(() => {
             this.masonry.reloadItems();
-            this.masonry.layout();  
+            this.masonry.layout();
         }, 500)
-    }   
+    }
     /**
      * Object key convert to string && toUpperCase
      * @param value 
      * @returns 
      */
-    keyAsString( value : any ){ return value.toUpperCase(); }
+    keyAsString(value: any) { return value.toUpperCase(); }
 
-    setActiveClassByLang( lang : any ){
+    setActiveClassByLang(lang: any) {
         let langStr = lang.toString();
-        return ( this.currentLanguage.value === lang ) ? "active": "";
+        return (this.currentLanguage.value === lang) ? "active" : "";
     }
 
-     private i : number = 0;
-    ngOnInit(): void {  
-        this.langService.getObsData().subscribe( (lang : any) => {
-            this.currentLanguage = lang;
-        })
 
-    //    setInterval( () => { 
-    //         this.masonry.reloadItems();
-    //         this.masonry.layout();  
-    //     }, 500)
-    }
 
-    getEventNameByLang( desc : any, lang : any ){ 
+    getEventNameByLang(desc: any, lang: any) {
         return desc[lang].name;
     }
 
-    getDescriptionByLang( desc : any, lang : any ) : string{ 
-        return desc[lang].description; 
+    getDescriptionByLang(desc: any, lang: any): string {
+        return desc[lang].description;
     }
     // getDescriptionByCurrentLang( data: any ){
     //     console.log( data );
     // }
 
+
+    addItem() {
+        this.cardsData?.push({ id: "HELLO" })
+        
+    }
+
+
+    private activeClassSetted : boolean = false;
+    layoutCompleteRender(e: any) : any {
+
+        if( this.currentLanguage.value != "" ) { this.activeClassSetted = false; }
+        if( this.currentLanguage.value != "" || this.activeClassSetted ) return;
+        
+        let button = document.querySelectorAll('.masonry-item .card .nav-item:first-child button');
+            button.forEach( (el) => { el.classList.add('active') } )    
+        
+        let content = document.querySelectorAll('.masonry-item .card .tab-content .tab-pane:first-child');
+            content.forEach((el) => { el.classList.add('active') })
+            
+        this.activeClassSetted = true;
+    }
+
+
+    doOtherStuff(e: any) {
+        // console.log("other")
+    }
+
+
 }
+
+
