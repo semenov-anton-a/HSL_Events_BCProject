c1b89e834140d46ee50a46a8f4b67a163c74eed5
Krista-Mari Katajisto <kristamari.katajisto@gmail.com>
2022-05-05 12:17:38 +0300

    File Edited

--- src/app/pages/index.ts
+++ src/app/pages/index.ts
@@ -1,3 +1,4 @@
+
 // import { WeatherComponent } from './../weather/weather.component';
 // import { WeatherComponent } from '../components/weather/weather.component';
 export { DetailsComponent } from './details/details.component';
