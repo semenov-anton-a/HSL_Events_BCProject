b26c74beb976f7812584a6ef69804dad527bf632
Anton Semenov <semenov.anton.a@gmail.com>
2022-05-05 14:15:02 +0300

    [ ADDed ] category list HOME

--- src/app/pages/favorites/favorites.component.ts
+++ src/app/pages/favorites/favorites.component.ts
@@ -1,15 +1,46 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-favorites',
-  templateUrl: './favorites.component.html',
-  styleUrls: ['./favorites.component.css']
+    selector: 'app-favorites',
+    templateUrl: './favorites.component.html',
+    styleUrls: ['./favorites.component.css']
 })
 export class FavoritesComponent implements OnInit {
 
-  constructor() { }
+    constructor() { }
 
-  ngOnInit(): void {
-  }
+    private readonly localStorageKeyName = 'favorites';
+
+    private fArray : any = new Array();
+
+
+    ngOnInit(): void {
+
+        let act = {"activity" : "actID"};
+        let act2 = {"activity" : "actID2"};
+        
+        
+        let evt = {"event" : "evtID"};
+        let evt2 = {"event" : "evtID2"};
+        
+
+
+        this.fArray.push( act )
+        this.fArray.push( act2 )
+        this.fArray.push( evt )
+        this.fArray.push( evt2 )
+
+        localStorage.setItem( this.localStorageKeyName , JSON.stringify(this.fArray) );
+
+
+        console.log("HELLO")
+
+
+
+        var storedNames : any = localStorage.getItem( this.localStorageKeyName );
+        let json = JSON.parse( storedNames );
+        
+        console.log( json )
+    }
 
 }
